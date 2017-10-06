f8c42bd1ab00f44bc0ca26526d49f76954fa2b12
ahmadnorin <ahmadnorin@gmailcom>
2017-10-06 10:32:48 +0700

    initial commit

---
+++ src/Hitung.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @Author: jdi-juma
+ * @Date:   2017-10-06 10:23:15
+ * @Last Modified by:   jdi-juma
+ * @Last Modified time: 2017-10-06 10:25:56
+ */
+namespace AhmadPackage\Hitung;
+Class Hitung {
+	
+	public function proses()
+	{
+		return 12;
+	}
+}
---
+++ test/Bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @Author: jdi-juma
+ * @Date:   2017-10-06 10:26:24
+ * @Last Modified by:   jdi-juma
+ * @Last Modified time: 2017-10-06 10:26:26
+ */
+<?php
+
+if (! @include_once __DIR__.'/../vendor/autoload.php') {
+  exit("You must set up the project dependencies, run the following commands:\n> wget http://getcomposer.org/composer.phar\n> php composer.phar install\n");
+}
